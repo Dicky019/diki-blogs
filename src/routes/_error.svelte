143c71d63e6f22908d26f688d7a6c6ccd65a169d
R4MA26 <ramaismaya25@gmail.com>
2022-02-16 15:48:01 +0800

    first commit

---
+++ src/routes/_error.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let status;
+  export let error;
+
+  const dev = process.env.NODE_ENV === "development";
+</script>
+
+<svelte:head>
+  <title>{status}</title>
+</svelte:head>
+
+<div class="center">
+  <h1>{status}</h1>
+  <p>{error.message}</p>
+</div>
+
+{#if dev && error.stack}
+  <pre>{error.stack}</pre>
+{/if}
+
+<style>
+  h1,
+  p {
+    margin: 0 auto;
+  }
+
+  .center {
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2.8em;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
+
+  p {
+    margin: 1em auto;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
+</style>
